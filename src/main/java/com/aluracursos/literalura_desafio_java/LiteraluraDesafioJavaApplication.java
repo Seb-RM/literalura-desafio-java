262e6821e5c41603751837e498d8d05cf2ecc9c3
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-11-15 18:03:09 -0300

    Primer Commit

---
+++ src/main/java/com/aluracursos/literalura_desafio_java/LiteraluraDesafioJavaApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.literalura_desafio_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraDesafioJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraDesafioJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/literalura_desafio_java/LiteraluraDesafioJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.literalura_desafio_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraDesafioJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
