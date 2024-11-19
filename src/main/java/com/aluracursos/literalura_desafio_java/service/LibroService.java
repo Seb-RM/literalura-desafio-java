5d6353304a7d9a126014c6ec3612dc566473b674
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-11-19 16:33:13 -0300

    actualizacion de LibroService para buscar libros por titulo

--- src/main/java/com/aluracursos/literalura_desafio_java/service/LibroService.java
+++ src/main/java/com/aluracursos/literalura_desafio_java/service/LibroService.java
@@ -1,16 +1,56 @@
 package com.aluracursos.literalura_desafio_java.service;
 
+import com.aluracursos.literalura_desafio_java.client.LibroApiClient;
+import com.aluracursos.literalura_desafio_java.model.Libro;
+import com.aluracursos.literalura_desafio_java.model.RespuestaApi;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class LibroService {
 
-    public List<String> buscarLibrosPorTitulo(String titulo) {
-        // Aquí implementas la lógica para buscar libros (puede ser llamada a la API o lógica simulada)
-        return List.of("Libro 1", "Libro 2"); // Ejemplo de datos simulados
+    private final LibroApiClient apiClient;
+    private final ConvierteDatos convierteDatos;
+    private final List<Libro> catalogoLibros = new ArrayList<>();
+
+    public LibroService(LibroApiClient apiClient, ConvierteDatos convierteDatos) {
+        this.apiClient = apiClient;
+        this.convierteDatos = convierteDatos;
     }
+
+    public Libro buscarLibroPorTitulo(String titulo) {
+        String queryParams = "?search=" + titulo.replace(" ", "+");
+        String jsonResponse = apiClient.getLibros(queryParams);
+
+        RespuestaApi respuestaApi = convierteDatos.convertJsonToApiResponse(jsonResponse);
+
+        if (respuestaApi.getResults().isEmpty()) {
+            System.out.println("⚠️ No se encontró ningún libro con ese título.");
+            return null;
+        }
+        Libro libro = respuestaApi.getResults().get(0);
+
+        if (!libro.getIdiomas().isEmpty()) {
+            libro.setIdiomas(List.of(libro.getIdiomas().get(0)));
+        }
+
+        catalogoLibros.add(libro);
+        return libro;
+    }
+
+    public List<Libro> listarTodosLosLibros() {
+        return new ArrayList<>(catalogoLibros);
+    }
+
+    public List<Libro> filtrarLibrosPorIdioma(String idioma) {
+        return catalogoLibros.stream()
+                .filter(libro -> !libro.getIdiomas().isEmpty() && libro.getIdiomas().get(0).equalsIgnoreCase(idioma))
+                .collect(Collectors.toList());
+    }
+
 
     public List<String> mostrarLibrosPopulares() {
         // Lógica para obtener libros populares
